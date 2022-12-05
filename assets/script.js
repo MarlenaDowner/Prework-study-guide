a7da9c0784e498fa86a5636a87662eaee90714ab
MarlenaDowner <119687938+MarlenaDowner@users.noreply.github.com>
2022-12-05 17:34:48 +0000

    commit

--- assets/script.js
+++ assets/script.js
@@ -14,7 +14,8 @@ var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
     }
   }
 
-  function listTopics() {
+ function listTopics() 
+{
     for (var x = 0; x < topics.length; x++) {
       console.log(topics[x]);
     }
